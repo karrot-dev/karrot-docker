29c5dfcb912c6198ec25c2c62d09cba6db10bbce
Andreas Langecker <a.langecker@posteo.de>
2018-02-17 22:50:42 +0100

    updated locale_settings.py

--- docker/backend/local_settings.py
+++ docker/backend/local_settings.py
@@ -13,8 +13,41 @@ DATABASES = {
     }
 }
 
+SECRET_KEY = 'c*#4=n$s4!*gdgq3nora#a$*xznctg-6=4_edeg9^dsxk&=p=$'
+DEBUG = True
+
+# needed for deployment
+STATIC_ROOT = './static/'
+ALLOWED_HOSTS = ['*']
+
+# Uploaded files location, e.g. user photos
+MEDIA_ROOT = './uploads/'
+MEDIA_URL = '/media/'
+
+######
+# Mail configuration
+# Uses the console backend for local development
+# Set the sparkpost key on deploy
+######
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# ANYMAIL = {
+#     "SPARKPOST_API_KEY": "your sparkpost key"
+# }
+
+DEFAULT_FROM_EMAIL = "fstool@yunity.org"  # can be anything if using the console backend
+HOSTNAME = 'https://localhost:8080'  # for constructing the frontend URL
+SITE_NAME = 'karrot.world' # used as human readable site name, for example in e-mail templates
+
+ 
+######
+# InfluxDB config for statistics reporting
+# Disabled by default
+######
+
 INFLUXDB_DISABLED = False
-INFLUXDB_HOST = 'influxdb'
+INFLUXDB_HOST = '127.0.0.1'
 INFLUXDB_PORT = '8086'
 INFLUXDB_USER = ''
 INFLUXDB_PASSWORD = ''
@@ -24,16 +57,25 @@ INFLUXDB_TIMEOUT = 2
 INFLUXDB_USE_CELERY = False
 INFLUXDB_USE_THREADING = True
 
-SHELL_PLUS_PRE_IMPORTS = (
-)
+######
+# Firebase Cloud Messaging config
+# For sending push messages
+# Get one from https://console.firebase.google.com
+######
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'maildev'
+FCM_SERVER_KEY = 'your server key'
 
-DEFAULT_FROM_EMAIL = "fstool@yunity.org"
+#######
+# Sentry.io config for error reporting
+# Only needs to be enabled on deploy
+########
 
-SECRET_KEY = 'c*#4=n$s4!*gdgq3nora#a$*xznctg-6=4_edeg9^dsxk&=p=$'
-DEBUG = True
-HOSTNAME = 'http://localhost:8000'
+# import os
+# import raven
 
-ALLOWED_HOSTS = ['*']
+# RAVEN_CONFIG = {
+#    'dsn': 'https://f4b3de91ad384e4ca0d371c591cf3904:60760787f4464c5293afa6fe877d3d9d@sentry.io/147126',
+#    # If you are using git, you can also automatically configure the
+#    # release based on the git info.
+#    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+#}
